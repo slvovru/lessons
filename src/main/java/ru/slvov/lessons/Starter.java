5fe3413bbd24401eef1198280f0cbdca3d5abb53
slvovru <i@slvov.ru>
2015-09-23 15:29:42 +0300

    Starter HelloWorld program

---
+++ src/main/java/ru/slvov/lessons/Starter.java
@@ -0,0 +1,12 @@
+package ru.slvov.lessons;
+
+/**
+ * Created by Serge on 23.09.2015.
+ *
+ * Just print Hello World
+ */
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
